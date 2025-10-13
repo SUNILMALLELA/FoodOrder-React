eaabba75950e520324bcfb7dd1b12fafefdee1c5
Sunil Kumar Reddy <T0053@VFG-DC.LOCAL>
2025-10-13 18:21:23 +0530

    sec

--- src/Components/FoodItem.jsx
+++ src/Components/FoodItem.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function FoodItem({ food }) {
     return (
         <>
-            <img src={food.image} alt=''></img>
-            <h1>{food.title}</h1>
+            <img width={'30px'} height={'30px'} src={food.image} alt=''></img>
+            <h1>{food.name}</h1>
             <button>View Recipe</button>
         </>
     )
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Search.module.css'
 function Search({ foodData, setFoodData }) {
-    const [query, setQuery] = useState("pizza");
-    useEffect(() => {
-        const URL = "https://api.spoonacular.com/recipes/complexSearch";
-        const API_KEY = "bfdfecc7f81547318a6694fe30624708";
-        async function fetchFood() {
-            try {
-                const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
-                const data = await res.json();
-                setFoodData(data.results)
-                console.log(data);
-
-
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
-
-        fetchFood();
-    }, [query]);
+    const [query, setQuery] = useState("");
     // useEffect(() => {
+    //     const URL = "https://api.spoonacular.com/recipes/complexSearch";
+    //     const API_KEY = "bfdfecc7f81547318a6694fe30624708";
     //     async function fetchFood() {
     //         try {
-    //             const res = await fetch('https://dummyjson.com/recipes')
-    //             const data = await res.json()
-    //             console.log("Data", data.recipes)
-    //             setFoodData(data.recipes)
-    //         }
-    //         catch (error) {
+    //             const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+    //             const data = await res.json();
+    //             setFoodData(data.results)
+    //             console.log(data);
+
+
+    //         } catch (error) {
     //             console.error("Error fetching data:", error);
     //         }
     //     }
-    //     fetchFood()
-    // }, [query])
+
+    //     fetchFood();
+    // }, [query]);
+    useEffect(() => {
+        async function fetchFood() {
+            try {
+                const res = await fetch(`https://dummyjson.com/recipes/search?q=${query}`)
+                const data = await res.json()
+                console.log("Data", data.recipes)
+                setFoodData(data.recipes)
+            }
+            catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+        fetchFood()
+    }, [query])
     return (
         <>
             <input
